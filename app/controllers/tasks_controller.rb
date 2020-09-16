1fb022998960b2698c749c562b36d9f58aead782
pedrohfs31 <pedrosampaioepr@gmail.com>
2020-09-16 14:44:11 -0300

    update task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,5 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show]
+  before_action :set_task, only: [:show, :edit, :update]
 
   def index
     @tasks = Task.all
@@ -10,7 +10,7 @@ class TasksController < ApplicationController
   end
 
   def new
-     @task = Task.new
+    @task = Task.new
   end
 
   def create
@@ -20,9 +20,17 @@ class TasksController < ApplicationController
     redirect_to task_path(@task)
   end
 
+  def edit
+
+  end
+
+  def update
+    @task.update(task_params)
+    redirect_to task_path(@task)
+
+  end
+
   private
-
-
 
   def set_task
     @task = Task.find(params[:id])
@@ -31,5 +39,4 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:title, :details, :completed)
   end
-
 end
