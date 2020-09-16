4682107b269a3f6ca9e23f5e1a99920f1a48190e
pedrohfs31 <pedrosampaioepr@gmail.com>
2020-09-16 14:25:02 -0300

    3 e 4 routes: new task

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -9,9 +9,27 @@ class TasksController < ApplicationController
     # @task = Task.find(params[:id]) # before_action
   end
 
+  def new
+     @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    @task.save
+
+    redirect_to task_path(@task)
+  end
+
   private
+
+
 
   def set_task
     @task = Task.find(params[:id])
   end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
+
 end
