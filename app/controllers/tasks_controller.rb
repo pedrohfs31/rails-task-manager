8b0e8f0eb1d14fbb4c48011be563fc1254eb59fb
pedrohfs31 <pedrosampaioepr@gmail.com>
2020-09-16 14:08:21 -0300

    2 routes: tasks#index e tasks#show

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,17 @@
 class TasksController < ApplicationController
+  before_action :set_task, only: [:show]
+
   def index
     @tasks = Task.all
+  end
+
+  def show
+    # @task = Task.find(params[:id]) # before_action
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
   end
 end
